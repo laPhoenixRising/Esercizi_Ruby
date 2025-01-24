b411699a6bb362b02cf3d8ec2628d62bc70a9f7d
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-24 10:25:23 +0100

    Esercizio 4

---
+++ ex4.rb
@@ -0,0 +1,11 @@
+print "inserisci una stringa: "
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+  user_input.gsub!(/s/, "th")
+else
+  print "Non è presente la lettera s"  
+end 
+
+puts "La parola è #{user_input}"
