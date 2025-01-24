7640b734c370fd11f5ac975a370d0d1d59e39e1a
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-24 10:27:41 +0100

    Inserito ritorno a capo

--- ex4.rb
+++ ex4.rb
@@ -5,7 +5,7 @@ user_input.downcase!
 if user_input.include? "s"
   user_input.gsub!(/s/, "th")
 else
-  print "Non è presente la lettera s"  
+  print "Non è presente la lettera s \n"  
 end 
 
 puts "La parola è #{user_input}"
