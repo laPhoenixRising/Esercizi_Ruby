aeff65cadf3e8c4ea3cfe3986b36504e4e13ed88
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-27 16:48:32 +0100

    Creare una matrice

---
+++ matrice.rb
@@ -0,0 +1,11 @@
+# creare una matrice 3x4 (3 righe x 4 colonne):
+# / 1 1 \
+# 1 0 0 1
+# \ 1 1 /
+
+a = ['/', 1, 1, '\\']
+puts a.join(" ")
+b = [1, 0, 0, 1]
+puts b.join(" ")
+c = ['\\', 1, 1, '/']
+puts c.join(" ")
