ad4e1f8abd3584f41be7af909434988b634620d4
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-27 16:17:35 +0100

    Rimuovere elementi da una lista. Utilizzati più metodi

---
+++ rimuovere_elemento.rb
@@ -0,0 +1,21 @@
+# togliere un elemento da una lista
+
+a = [1, 4, 5, 78, 43, 12]
+a.pop
+print a
+puts
+
+a = [1, 4, 5, 78, 43, 12]
+a.shift
+print a
+puts
+
+a = [1, 4, 5, 78, 43, 12]
+a.delete_at(2)
+print a
+puts
+
+a = [1, 4, 5, 78, 43, 12]
+a.delete(78)
+print a
+puts
