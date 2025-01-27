cfc441f5ba6a6131114601e0c91f56e392e82b1d
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-27 17:50:48 +0100

    aggiungere all'hash una lista dei 5 film preferiti

---
+++ aggiunta_hash.rb
@@ -0,0 +1,5 @@
+# aggiungere all'hash una lista dei 5 film preferiti
+
+a = {nome: "Francesca", cognome: "Colombo", età: 36}
+a[:film] = "Young Shedon", "Titanic", "Ghost", "Amelie", "Sandman"
+puts a
