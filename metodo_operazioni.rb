2d67b05c64c52851da3fab78192bbaedd5dc25e4
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-28 11:41:38 +0100

    Creazione metodi restituzione risultati operazioni

---
+++ metodo_operazioni.rb
@@ -0,0 +1,22 @@
+# definire 4 metodi, uno per ogni operazione matematica, che accettano due numeri e restituiscono il risultato dell'operazione
+
+def somma(a, b)
+    s = a + b
+    return s
+end
+
+def sottrazione(a, b)
+    s = a - b
+    return s
+end
+
+def moltiplicazione(a, b)
+    m = a * b
+    return m
+end
+
+def divisione(a, b)
+    d = a / b
+    return d
+end    
+
