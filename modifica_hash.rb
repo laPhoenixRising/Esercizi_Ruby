812cad67696f89c137667868897f1be37688fee9
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-28 22:43:06 +0100

    Rifattorizzazione

--- modifica_hash.rb
+++ modifica_hash.rb
@@ -1,6 +1,11 @@
 # Dell'hash modificare il nome del 2° film preferito e stampare il nome del 3° film preferito
 
-a = {nome: "Francesca", cognome: "Colombo", età: 36, film: ["Young Shedon", "Titanic", "Ghost", "Amelie", "Sandman"]}
+a = { 
+  nome: "Francesca",
+  cognome: "Colombo",
+  età: 36,
+  film: [ "Young Shedon", "Titanic", "Ghost", "Amelie", "Sandman" ]
+}
 a[:film][1] = "The big bang theory"
 puts a
 
