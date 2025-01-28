167b1e8febf98cde92962f0da8b2e2365179f3c8
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-28 11:24:46 +0100

    Creazione metodo saluto e ritorno della lunghezza

---
+++ metodo_saluto_e_ritorno_lunghezza.rb
@@ -0,0 +1,10 @@
+# definire un metodo che non accetta alcun parametro stampa un saluto e ritorna la lunghezza dei caratteri del saluto
+
+def saluta
+    a = "ciao"
+    puts a
+    l = a.length
+    return l
+end    
+
+saluta
