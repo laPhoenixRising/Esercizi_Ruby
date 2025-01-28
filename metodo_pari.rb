0c9b299ee665abfc049f6f190344bffc7da3bbce
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-28 22:37:49 +0100

    Rifattorizzazione

--- metodo_pari.rb
+++ metodo_pari.rb
@@ -1,9 +1,3 @@
 # Crea un metodo che accetta un numero come parametro e restituisce vero se il numero è pari, falso se è dispari
 
-def pari_dispari(num)
-    if num % 2 == 0
-        return true
-    else
-        return false
-    end
-end            
+def pari_dispari(num) = num % 2 == 0
