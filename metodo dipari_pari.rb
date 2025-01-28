2b8dc896f72aa9d52d51754db42f1f69bf295488
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-28 14:43:03 +0100

    Creazione di un metodo che restituisce true se il numero è dispari e false se il numero è pari

---
+++ metodo dipari_pari.rb
@@ -0,0 +1,9 @@
+# Crea un metodo che accetta un numero come parametro e restituisce vero se il numero è dispari, falso se è pari
+
+def dispari_pari(num)
+    if num % 2 == 1
+        return true
+    else
+        return false
+    end
+end
