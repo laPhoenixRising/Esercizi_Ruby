77e3b1d0cee9cb8b23b264dde6883493e115cc21
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-30 12:06:58 +0100

    Metodo che accetta un numero come parametro, lo moltiplica per 2 e stampa a schermo un messaggio solo se il risultato è maggiore di 10

---
+++ metodo_maggiore_10.rb
@@ -0,0 +1,7 @@
+# crea un metodo che accetta un numero come parametro, 
+# lo moltiplica per 2 e stampa a schermo un messaggio solo se il risultato è maggiore di 10
+
+def exercise(num)
+  num * 2
+  puts "Il numero #{num} è maggiore di 10 " if num > 10
+end
