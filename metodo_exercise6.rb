8497a94bf010236c8ab210c02952c156ee175647
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-30 13:28:07 +0100

    Metodo che accetta un numero con decimali scritto come stringa,lo converte in numero con decimali e lo restituisce diviso per 10

---
+++ metodo_exercise6.rb
@@ -0,0 +1,4 @@
+# crea un metodo che accetta un numero con decimali scritto come stringa (ad esempio "42.23"), 
+# lo converte in numero con decimali (float) e lo restituisce diviso per 10
+
+def exercise(str) = str.to_f / 10
