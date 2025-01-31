fe7398ed9294bdbc62ae10546f556df5d2b02b52
LaPhoenixRising <laphoenixrising88@gmail.com>
2025-01-31 18:56:06 +0100

    Rifattorizzazione

--- metodo_exercise13.rb
+++ metodo_exercise13.rb
@@ -1,16 +1,18 @@
 # Creare un hash che contiene informazioni come nome, cognome, età e una lista di 5 film preferiti 
-# e ottiene in un'altra variabile la conversione in JSON dell'hashe salvarla in un file
+# e ottiene in un'altra variabile la conversione in JSON dell'hash e salvarla in un file
 
-def dati
-  require 'json'
-  h = {
-    first_name: "Francesca", 
-    last_name: "Colombo", 
-    age: 36, 
-    film: ["Titanic", "Ghost", "Young Sheldon", "Sandman"]
-  }
+require 'json'
+
+def dati(h)
   json_string = JSON.pretty_generate(h)
   File.write("file_json.txt", json_string)  
 end
 
-dati
+h = {
+  first_name: "Francesca", 
+  last_name: "Colombo", 
+  age: 36, 
+  film: ["Titanic", "Ghost", "Young Sheldon", "Sandman"]
+}
+
+dati(h)
